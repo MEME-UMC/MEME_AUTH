e91968172e5e5d96e121809341bee5accd4f69a7
sunwupark <sunwu5678@gmail.com>
2024-01-29 12:28:03 +0900

    :add User Services and Repository

--- src/main/java/umc/meme/auth/domain/user/domain/UserRepository.java
+++ src/main/java/umc/meme/auth/domain/user/domain/UserRepository.java
@@ -2,6 +2,7 @@ package umc.meme.auth.domain.user.domain;
 
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
@@ -9,9 +10,17 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
-    @Query(value = "insert into artist values (username = :username, email = :email) ", nativeQuery = true)
+    @Modifying
+    @Query(value = "insert into Artist values (username = :username, email = :email) ", nativeQuery = true)
     void saveArtist(@Param("username") String username, @Param("email") String email);
 
-    @Query(value = "insert into model values (username = :username, email = :email) ", nativeQuery = true)
+    @Modifying
+    @Query(value = "insert into Aodel values (username = :username, email = :email) ", nativeQuery = true)
     void saveModel(@Param("username") String username, @Param("email") String email);
+
+    @Query(value = "select a from Artist a where a.username = :username")
+    void findArtistByUsername(@Param("username") String username);
+
+    @Query(value = "select m from Model m where m.username = :username")
+    void findModelByUsername(@Param("username") String username);
 }
--- src/main/java/umc/meme/auth/domain/user/service/UserService.java
+++ src/main/java/umc/meme/auth/domain/user/service/UserService.java
@@ -1,8 +1,12 @@
 package umc.meme.auth.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import umc.meme.auth.domain.user.domain.Artist;
+import umc.meme.auth.domain.user.domain.ArtistRepository;
+import umc.meme.auth.domain.user.domain.User;
 import umc.meme.auth.domain.user.domain.UserRepository;
 import umc.meme.auth.domain.user.dto.UserRequest;
 
@@ -11,11 +15,19 @@ import umc.meme.auth.domain.user.dto.UserRequest;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final ArtistRepository artistRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public void signup(UserRequest.JoinDto joinDto) {
-        if (joinDto.getRole() == "ARTIST") {
-            userRepository.saveArtist(joinDto.getUsername(), joinDto.getEmail());
+        if (joinDto.getRole().startsWith("ARTIST") ){
+            Artist user = Artist.builder()
+                    .email(joinDto.getEmail())
+                    .username(joinDto.getUsername())
+                    .password(passwordEncoder.encode(joinDto.getEmail()))
+                    .role("ARTIST")
+                    .build();
+            artistRepository.save(user);
         } else {
             userRepository.saveModel(joinDto.getUsername(), joinDto.getEmail());
         }
