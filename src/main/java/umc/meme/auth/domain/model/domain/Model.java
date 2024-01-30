e46eacb6c2993eb3adb5636010eb77d2360e8490
sunwupark <sunwu5678@gmail.com>
2024-01-30 23:17:15 +0900

    :Refactor Model

--- src/main/java/umc/meme/auth/domain/model/domain/Model.java
+++ src/main/java/umc/meme/auth/domain/model/domain/Model.java
@@ -1,13 +1,16 @@
 package umc.meme.auth.domain.model.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import umc.meme.auth.domain.artist.domain.enums.Gender;
+import umc.meme.auth.domain.model.domain.enums.PersonalColor;
+import umc.meme.auth.domain.model.domain.enums.SkinType;
 import umc.meme.auth.domain.user.domain.User;
+
+import java.util.Date;
 
 @SuperBuilder
 @Getter
@@ -17,15 +20,26 @@ import umc.meme.auth.domain.user.domain.User;
 @Entity
 public class Model extends User {
 
-    @Column(nullable = true)
-    private String gender;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
 
-    @Column(nullable = true, length = 40)
+    @Column(nullable = false, length = 40)
     private String nickname;
 
-    @Column(nullable = true)
+    @Column(nullable = false)
     private String profileImg;
 
     @Column(nullable = true, length = 500)
-    private String introduction = "안녕하세요! 저는 ___입니다!";
+    private String introduction;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SkinType skinType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PersonalColor personalColor;
+
+    private Date inactive;
 }
--- src/main/java/umc/meme/auth/global/auth/PrincipalDetailsService.java
+++ src/main/java/umc/meme/auth/global/auth/PrincipalDetailsService.java
@@ -5,19 +5,33 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
+import umc.meme.auth.domain.artist.domain.Artist;
+import umc.meme.auth.domain.artist.domain.ArtistRepository;
+import umc.meme.auth.domain.model.domain.Model;
+import umc.meme.auth.domain.model.domain.ModelRepository;
 import umc.meme.auth.domain.user.domain.User;
 import umc.meme.auth.domain.user.domain.UserRepository;
+import umc.meme.auth.global.common.status.ErrorStatus;
+import umc.meme.auth.global.exception.handler.AritstHandler;
+import umc.meme.auth.global.exception.handler.MemberHandler;
+import umc.meme.auth.global.exception.handler.ModelHandler;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class PrincipalDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final ArtistRepository artistRepository;
+    private final ModelRepository modelRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with the name " + username));
+                .orElseThrow(() -> new MemberHandler(ErrorStatus.MEMBER_NOT_FOUND));
+
         return new PrincipalDetails(user);
     }
 }
