db90e6fb7ae90c2506feb5280162d75b3b04aa4e
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-02-05 03:01:28 +0900

    :add Swagger to Auth

--- src/main/java/umc/meme/auth/global/auth/AuthController.java
+++ src/main/java/umc/meme/auth/global/auth/AuthController.java
@@ -1,5 +1,6 @@
 package umc.meme.auth.global.auth;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,24 +16,27 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "소셜 로그인", description = "소셜 로그인할때 사용하는 API입니다.")
     @PostMapping("/api/v0/auth/login")
     public ResponseEntity<?> login(@RequestBody AuthRequest.LoginDto loginDto) {
         return ResponseEntity.ok(authService.login(loginDto));
     }
 
+    @Operation(summary = "토큰 재발급", description = "토큰이 만료가 되었을때 재발급을 받는 API입니다")
     @PostMapping("/api/v0/auth/reissue")
     public ResponseEntity<?> reissue(@RequestBody AuthRequest.ReissueDto reissueDto) {
         return ResponseEntity.ok(authService.reissue(reissueDto));
     }
 
+    @Operation(summary = "로그아웃", description = "로그아웃하는 API입니다")
     @PostMapping("/api/v0/auth/logout")
     public ResponseEntity<?> logout(@RequestHeader("Authorization") AuthRequest.AccessTokenDto requestAccessTokenDto) {
         authService.logout(requestAccessTokenDto);
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
-    @PostMapping("/api/v0/auth/withdraw")
-    public ResponseEntity<?> withdraw(@RequestHeader("Authorization") AuthRequest.AccessTokenDto requestAccessTokenDto) {
-        authService
-    }
+//    @PostMapping("/api/v0/auth/withdraw")
+//    public ResponseEntity<?> withdraw(@RequestHeader("Authorization") AuthRequest.AccessTokenDto requestAccessTokenDto) {
+//        authService
+//    }
 }
