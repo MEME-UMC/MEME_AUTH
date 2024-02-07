d03d5be97d9f91a735476879a231f0b9d12ddfc8
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-02-07 13:51:58 +0900

    :fix SecurityConfig

--- src/main/java/umc/meme/auth/global/config/SecurityConfig.java
+++ src/main/java/umc/meme/auth/global/config/SecurityConfig.java
@@ -1,83 +1,83 @@
-//package umc.meme.auth.global.config;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//import umc.meme.auth.global.jwt.JwtAccessDeniedHandler;
-//import umc.meme.auth.global.jwt.JwtCustomAuthenticationFilter;
-//import umc.meme.auth.global.auth.PrincipalDetailsService;
-//import umc.meme.auth.global.jwt.JwtAuthenticationEntryPoint;
-//
-//import static org.springframework.security.config.http.SessionCreationPolicy.*;
-//
-//@RequiredArgsConstructor
-//@EnableWebSecurity
-//@Configuration
-//public class SecurityConfig {
-//
-//    private final PrincipalDetailsService userDetailService;
-//    private final JwtCustomAuthenticationFilter jwtCustomAuthenticationFilter;
-//    private final JwtAuthenticationEntryPoint authenticationEntryPoint;
-//    private final JwtAccessDeniedHandler accessDeniedHandler;
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .sessionManagement((sessionManagement) ->
-//                        sessionManagement.sessionCreationPolicy(STATELESS)
-//                )
-//                .formLogin(AbstractHttpConfigurer::disable)
-//                .httpBasic(AbstractHttpConfigurer::disable)
-//                .exceptionHandling((exceptionHandling) ->
-//                        exceptionHandling
-//                                .authenticationEntryPoint(authenticationEntryPoint)  // 401
-//                                .accessDeniedHandler(accessDeniedHandler)  // 403
-//                )
-//                .authorizeHttpRequests((authorizeHttpRequests) ->
-//                        authorizeHttpRequests
-//                                .requestMatchers("/api/v0/auth/model/signup").permitAll()
-//                                .requestMatchers("/api/v0/auth/artist/signup").permitAll()
-//                                .requestMatchers("/api/v0/auth/artist/extra").permitAll()
-//                                .requestMatchers("/api/v0/auth/login").permitAll()
-//                                .requestMatchers("/api/v0/auth/logout").permitAll()
-//                                .requestMatchers("/api/v0/auth/withdraw").permitAll()
-//                                .requestMatchers("/api/v0/auth/reissue").permitAll()
-//                                .requestMatchers("/auth/**").permitAll()
-//                );
-//
-//
-//        http.addFilterBefore(jwtCustomAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-//        return authenticationConfiguration.getAuthenticationManager();
-//    }
-//
-//    @Bean
-//    public DaoAuthenticationProvider daoAuthenticationProvider() throws Exception {
-//        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-//
-//        daoAuthenticationProvider.setUserDetailsService(userDetailService);
-//        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-//
-//        return daoAuthenticationProvider;
-//    }
-//
-//    @Bean
-//    public static BCryptPasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//}
+package umc.meme.auth.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import umc.meme.auth.global.jwt.JwtAccessDeniedHandler;
+import umc.meme.auth.global.jwt.JwtCustomAuthenticationFilter;
+import umc.meme.auth.global.auth.PrincipalDetailsService;
+import umc.meme.auth.global.jwt.JwtAuthenticationEntryPoint;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.*;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final PrincipalDetailsService userDetailService;
+    private final JwtCustomAuthenticationFilter jwtCustomAuthenticationFilter;
+    private final JwtAuthenticationEntryPoint authenticationEntryPoint;
+    private final JwtAccessDeniedHandler accessDeniedHandler;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(STATELESS)
+                )
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .exceptionHandling((exceptionHandling) ->
+                        exceptionHandling
+                                .authenticationEntryPoint(authenticationEntryPoint)  // 401
+                                .accessDeniedHandler(accessDeniedHandler)  // 403
+                )
+                .authorizeHttpRequests((authorizeHttpRequests) ->
+                        authorizeHttpRequests
+                                .requestMatchers("/api/v0/auth/model/signup").permitAll()
+                                .requestMatchers("/api/v0/auth/artist/signup").permitAll()
+                                .requestMatchers("/api/v0/auth/artist/extra").permitAll()
+                                .requestMatchers("/api/v0/auth/login").permitAll()
+                                .requestMatchers("/api/v0/auth/logout").permitAll()
+                                .requestMatchers("/api/v0/auth/withdraw").permitAll()
+                                .requestMatchers("/api/v0/auth/reissue").permitAll()
+                                .requestMatchers("/auth/**").permitAll()
+                );
+
+
+        http.addFilterBefore(jwtCustomAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() throws Exception {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+
+        daoAuthenticationProvider.setUserDetailsService(userDetailService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
