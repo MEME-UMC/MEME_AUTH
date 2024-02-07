9dbc793ea46b0af547973d61ebcb11ff7a34c655
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-02-07 13:56:39 +0900

    :fix SwaggerConfig

--- src/main/java/umc/meme/auth/global/config/SwaggerConfig.java
+++ src/main/java/umc/meme/auth/global/config/SwaggerConfig.java
@@ -32,7 +32,7 @@ public class SwaggerConfig {
                         .bearerFormat("JWT"));
 
         return new OpenAPI()
-                .addServersItem(new Server().url("/"))
+                .addServersItem(new Server().url("/auth/swagger-ui/"))
                 .info(info)
                 .addSecurityItem(securityRequirement)
                 .components(components);
