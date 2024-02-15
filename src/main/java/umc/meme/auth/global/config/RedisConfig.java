0db3aca93a6325fe6d15813fb08c550703bed605
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-02-16 01:27:05 +0900

    :fix change redis with RedisClusterConfiguration

--- src/main/java/umc/meme/auth/global/config/RedisConfig.java
+++ src/main/java/umc/meme/auth/global/config/RedisConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -23,7 +24,9 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration();
+        clusterConfiguration.clusterNode("host", port);
+        return new LettuceConnectionFactory(clusterConfiguration);
     }
 
     @Bean
