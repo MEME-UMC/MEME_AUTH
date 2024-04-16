8d208824d38eba9577a70522c33c1b377acd61c4
Jaeyoung Lim <limjyjustin@gmail.com>
2024-04-16 11:23:18 +0900

    chore: 불필요한 import 제거

--- src/main/java/umc/meme/auth/global/auth/AuthController.java
+++ src/main/java/umc/meme/auth/global/auth/AuthController.java
@@ -2,8 +2,6 @@ package umc.meme.auth.global.auth;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
