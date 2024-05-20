63182733442b07cb97119a6bf577d4e0b0774122
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-05-21 01:25:27 +0900

    :fix changing build.gradle to testImplementation

--- src/main/java/umc/meme/auth/global/config/EmbeddedRedisConfig.java
+++ src/main/java/umc/meme/auth/global/config/EmbeddedRedisConfig.java
@@ -1,130 +1,130 @@
-//package umc.meme.auth.global.config;
-//
-//import jakarta.annotation.PostConstruct;
-//import jakarta.annotation.PreDestroy;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.io.ClassPathResource;
-//import org.springframework.util.StringUtils;
-//import redis.embedded.RedisServer;
-//
-//import java.io.BufferedReader;
-//import java.io.File;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.util.Objects;
-//
-//@Configuration
-//public class EmbeddedRedisConfig {
-//
-//    @Value("${spring.data.redis.port}")
-//    private int redisPort;
-//    private RedisServer redisServer;
-//
-//    @PostConstruct
-//    public void redisServer() throws IOException {
-//        int port = isRedisRunning() ? findAvailablePort() : redisPort;
-//
-//        if (isArmMac()) {
-//            redisServer = new RedisServer(getRedisFileForArcMac(), port);
-//        } else {
-//            System.out.println("------------IS NOT MAC!!!!-------");
-//            redisServer = RedisServer.builder()
-//                    .port(redisPort)
-//                    .setting("maxmemory 128M")
-//                    .build();
-//        }
-//
-//        redisServer.start();
-//    }
-//
-//    @PreDestroy
-//    public void stopRedis() {
-//        if (redisServer != null)
-//            redisServer.stop();
-//    }
-//
-//    /**
-//     * Embedded Redis가 현재 실행중인지 확인
-//     */
-//    private boolean isRedisRunning() throws IOException {
-//        return isRunning(executeGrepProcessCommand(redisPort));
-//    }
-//
-//    /**
-//     * 현재 PC/서버에서 사용가능한 포트 조회
-//     */
-//    public int findAvailablePort() throws IOException {
-//
-//        for (int port = 10000; port <= 65535; port++) {
-//            Process process = executeGrepProcessCommand(port);
-//            if (!isRunning(process)) {
-//                return port;
-//            }
-//        }
-//
-//        throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
-//    }
-//
-//    /**
-//     * 해당 port를 사용중인 프로세스 확인하는 sh 실행
-//     */
-//    private Process executeGrepProcessCommand(int port) throws IOException {
-//        String command = String.format("netstat -nat | grep LISTEN|grep %d", port);
-//        String[] shell = {"/bin/sh", "-c", command};
-//        return Runtime.getRuntime().exec(shell);
-//    }
-//
-//    /**
-//     * 해당 Process가 현재 실행중인지 확인
-//     */
-//    private boolean isRunning(Process process) {
-//        String line;
-//        StringBuilder pidInfo = new StringBuilder();
-//
-//        try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-//
-//            while ((line = input.readLine()) != null) {
-//                pidInfo.append(line);
-//            }
-//
-//        } catch (Exception e) {
-//        }
-//
-//        return StringUtils.hasText(pidInfo.toString());
-//    }
-//
-//    /**
-//     * 현재 시스템이 ARM 아키텍처를 사용하는 MAC인지 확인
-//     * System.getProperty("os.arch") : JVM이 실행되는 시스템 아키텍처 반환
-//     * System.getProperty("os.name") : 시스템 이름 반환
-//     */
-//    private boolean isArmMac() {
-//        return Objects.equals(System.getProperty("os.arch"), "aarch64")
-//                && Objects.equals(System.getProperty("os.name"), "Mac OS X");
-//    }
-//
-//    private boolean isArmRaspberry(){
-//        return Objects.equals(System.getProperty("os.arch"), "arm")
-//                && Objects.equals(System.getProperty("os.name"), "Linux");
-//    }
-//
-//    /**
-//     * ARM 아키텍처를 사용하는 Mac에서 실행할 수 있는 Redis 바이너리 파일을 반환
-//     */
-//    private File getRedisFileForArcMac() {
-//        try {
-//            return new ClassPathResource("redis/redis-server-7.2.3-mac-arm64").getFile();
-//        } catch (Exception e) {
-//            throw new IllegalArgumentException();
-//        }
-//    }
-//
-//    private File getRedisFileForArmRaspberry(){
-//        try{
-//            return new ClassPathResource("redis/redis-server-7.2.3-linux-arm64").getFile();
-//        } catch (Exception e){
-//            throw new IllegalArgumentException();
-//        }
-//    }
-//}
+package umc.meme.auth.global.config;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.StringUtils;
+import redis.embedded.RedisServer;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
+@Configuration
+public class EmbeddedRedisConfig {
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void redisServer() throws IOException {
+        int port = isRedisRunning() ? findAvailablePort() : redisPort;
+
+        if (isArmMac()) {
+            redisServer = new RedisServer(getRedisFileForArcMac(), port);
+        } else {
+            System.out.println("------------IS NOT MAC!!!!-------");
+            redisServer = RedisServer.builder()
+                    .port(redisPort)
+                    .setting("maxmemory 128M")
+                    .build();
+        }
+
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null)
+            redisServer.stop();
+    }
+
+    /**
+     * Embedded Redis가 현재 실행중인지 확인
+     */
+    private boolean isRedisRunning() throws IOException {
+        return isRunning(executeGrepProcessCommand(redisPort));
+    }
+
+    /**
+     * 현재 PC/서버에서 사용가능한 포트 조회
+     */
+    public int findAvailablePort() throws IOException {
+
+        for (int port = 10000; port <= 65535; port++) {
+            Process process = executeGrepProcessCommand(port);
+            if (!isRunning(process)) {
+                return port;
+            }
+        }
+
+        throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
+    }
+
+    /**
+     * 해당 port를 사용중인 프로세스 확인하는 sh 실행
+     */
+    private Process executeGrepProcessCommand(int port) throws IOException {
+        String command = String.format("netstat -nat | grep LISTEN|grep %d", port);
+        String[] shell = {"/bin/sh", "-c", command};
+        return Runtime.getRuntime().exec(shell);
+    }
+
+    /**
+     * 해당 Process가 현재 실행중인지 확인
+     */
+    private boolean isRunning(Process process) {
+        String line;
+        StringBuilder pidInfo = new StringBuilder();
+
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+
+            while ((line = input.readLine()) != null) {
+                pidInfo.append(line);
+            }
+
+        } catch (Exception e) {
+        }
+
+        return StringUtils.hasText(pidInfo.toString());
+    }
+
+    /**
+     * 현재 시스템이 ARM 아키텍처를 사용하는 MAC인지 확인
+     * System.getProperty("os.arch") : JVM이 실행되는 시스템 아키텍처 반환
+     * System.getProperty("os.name") : 시스템 이름 반환
+     */
+    private boolean isArmMac() {
+        return Objects.equals(System.getProperty("os.arch"), "aarch64")
+                && Objects.equals(System.getProperty("os.name"), "Mac OS X");
+    }
+
+    private boolean isArmRaspberry(){
+        return Objects.equals(System.getProperty("os.arch"), "arm")
+                && Objects.equals(System.getProperty("os.name"), "Linux");
+    }
+
+    /**
+     * ARM 아키텍처를 사용하는 Mac에서 실행할 수 있는 Redis 바이너리 파일을 반환
+     */
+    private File getRedisFileForArcMac() {
+        try {
+            return new ClassPathResource("redis/redis-server-7.2.3-mac-arm64").getFile();
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private File getRedisFileForArmRaspberry(){
+        try{
+            return new ClassPathResource("redis/redis-server-7.2.3-linux-arm64").getFile();
+        } catch (Exception e){
+            throw new IllegalArgumentException();
+        }
+    }
+}
